25765ebfedb0168eafae3f1a305ae111329e1226
jaqp7363 <57783976+jaqp7363@users.noreply.github.com>
2020-01-12 19:48:53 +0900

    주문서비스 테스트 반영 - 주문취소, 재고수량 초과

--- jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import javax.persistence.EntityManager;
 
@@ -17,6 +18,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockExceptioin;
 import jpabook.jpashop.repository.OrderRepository;
 
 @RunWith(SpringRunner.class)
@@ -60,12 +62,46 @@ public class OrderServiceTest {
 	
 	@Test
 	public void 주문취소() throws Exception {
+		Member member = createMember("테스팅");
+		Book item = createItem("곧 취소", 500, 100);
 		
+		int orderCount = 2;
+		
+		Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+		
+		orderService.cancelOrder(orderId);
+		
+		Order order = orderRepository.findOne(orderId);
+		assertEquals("주문 상태는 취소", OrderStatus.CANCEL, order.getStatus());
+		assertEquals("item수량 원복", 100, item.getStockQuantity());
 	}
 	
-	@Test
+	@Test(expected = NotEnoughStockExceptioin.class)
 	public void 상품주문_재고수량초과() throws Exception {
+		Member member = createMember("고운정");
+		Item book = createItem("농업인의 삶", 5000, 2);
 		
+		int orderCount = 11;
+		
+		orderService.order(member.getId(), book.getId(), orderCount);
+		
+		fail("재고수량 부족");
 	}
 
+	public Member createMember(String name) {
+		Member member = new Member();
+		member.setName(name);
+		member.setAddress(new Address("서울", "잠실", "123-123"));
+		em.persist(member);
+		return member;
+	}
+	
+	public Book createItem(String name, int price, int qtt) {
+		Book book = new Book();
+		book.setName(name);
+		book.setPrice(price);
+		book.setStockQuantity(qtt);
+		em.persist(book);
+		return book;
+	}
 }
