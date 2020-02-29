e725d98e51d5a61d7c72142751c31f89c50d1885
jaqp7363 <57783976+jaqp7363@users.noreply.github.com>
2020-02-29 11:01:13 +0900

    상품 등록 및 수정 반영

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,9 +1,12 @@
 package jpabook.jpashop.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -25,7 +28,10 @@ public class MemberController {
 	}
 	
 	@PostMapping("/members/new")
-	public String create(@Valid MemberForm form) {
+	public String create(@Valid MemberForm form, BindingResult result) {
+		if(result.hasErrors()) {
+			return "members/createMemberForm";
+		}
 		Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
 		
 		Member member = new Member();
@@ -34,5 +40,13 @@ public class MemberController {
 		
 		memberService.join(member);
 		return "redirect:/";
+	}
+	
+	@GetMapping("members")
+	public String list(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
+		
 	}
 }
